27a9835e9cfc0bd160343853dc542f290ee528fa
sunaaank <suna.namgung@gmail.com>
2021-06-30 01:19:30 +0900

    Add: delete 아이콘 webfont 등록, 카드 scroll 기능 추가

--- javascript/shopping_list/main.js
+++ javascript/shopping_list/main.js
@@ -4,28 +4,59 @@
 
 // 삭제버튼 누르면 해당 id의 card 삭제 됨
 
-const input = document.querySelector('.list-input')
+const cards = document.querySelector('.cards')
+const input = document.querySelector('.footerInput')
+const addBtn = document.querySelector('.submitBtn')
 
-
-function addCard() {
-  const currentValue = input.value
-  const ul = document.querySelector('.card-list')
-  const li = document.createElement('li')
-  li.setAttribute('class', 'card')
-  li.innerHTML = `
-      <p>${currentValue}</p>
-      <div class="btns">
-        <button class="like-card">💖</button>
-        <button class="delete-card">❌</button>
-      </div>
-  `
-  ul.appendChild(li)
+function onReset() {
   input.value = "";
+} 
+
+function onAdd() {
+  const text = input.value
+  if (text === " ") {
+    onReset()
+    return;
+  }
+
+  const card = createCard(text)
+  cards.appendChild(card)
+  card.scrollIntoView({ block: 'center' })
+  onReset()
+  input.focus();
 }
 
-function handleSubmit() {
-  if (window.event.keyCode == 13) {
-      console.log("💥💥💥💥")
-      addCard()
-    }
+function createCard(text) {
+  const card = document.createElement('li')
+  card.setAttribute('class', 'card')
+  const p = document.createElement('p')
+  p.innerText = text;
+  const btns = document.createElement('div')
+  btns.setAttribute('class', 'btns')
+  const likeBtn = document.createElement('button')
+  likeBtn.setAttribute('class', likeBtn) 
+  likeBtn.innerHTML = `💖`
+  const deleteBtn = document.createElement('button')
+  deleteBtn.setAttribute('class', deleteBtn) 
+  deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`
+  deleteBtn.addEventListener('click', () => {
+    cards.removeChild(card)
+  })
+
+  card.appendChild(p)
+  card.appendChild(btns)
+  btns.appendChild(likeBtn)
+  btns.appendChild(deleteBtn)
+  return card
 }
+
+addBtn.addEventListener('click', () => {
+  onAdd()
+})
+
+input.addEventListener('keypress', () => {
+  if (window.event.keyCode == 13) {
+    console.log("💥💥💥💥")
+    onAdd()
+  }
+})
