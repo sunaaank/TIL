b57baa7c105132333bca870b8f8119c51987fee7
sunaaank <suna.namgung@gmail.com>
2021-07-28 00:42:39 +0900

    refactor: update bouncing ball to es6 class

--- javascript/bouncing_balls/main.js
+++ javascript/bouncing_balls/main.js
@@ -15,64 +15,59 @@ function random(min, max) {
 
 // define Ball constructor
 
-function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
-}
-
-// define ball draw method
-
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-// define ball update method
-
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
+class Ball{
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
 
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
   }
 
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
+  update() {
+    if((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }; 
 
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-};
-
-// define ball collision detection
-
-Ball.prototype.collisionDetect = function() {
-  for(let j = 0; j < balls.length; j++) {
-    if(!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+  collisionDetect() {
+    for(let j = 0; j < balls.length; j++) {
+      if(!(this === balls[j])) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+        }
       }
     }
   }
-};
-
-// define array to store balls and populate it
+}
 
 let balls = [];
 
